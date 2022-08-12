f5a86561bebd55e2027b79e89168966f6dc31b7b
Zbigniew Kulczński <z-kulczynski@o2.pl>
2022-08-12 10:25:01 +0200

    exponentiation and log addeed to main.js

--- main.js
+++ main.js
@@ -36,4 +36,13 @@ switch (operation) {
     case '/':
         action = previous / current
         break
-}
+    case '√':
+        action = Math.exp(previous, 1/ current)
+        break
+    case 'log':
+        action = Math.log(previous) / Math.log(current)
+        break
+        default:
+            return
+
+    }
